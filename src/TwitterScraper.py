d3e56fab9cfd2fd83eb4558df860976d243f91c8
meebs <alex@takayamasan.com>
2019-08-14 17:14:17 -0400

    tested file reading.

--- src/TwitterScraper.py
+++ src/TwitterScraper.py
@@ -1,33 +1,31 @@
-# import tweepy
-# import datetime, time
+import tweepy
+import datetime, time
 import json
 
 key_file = open('./secretKeys.json', 'r')
 keys = json.loads(key_file.read())
 
-print(keys['consumer_key'])
-
-# auth = tweepy.OAuthHandler(keys['consumer_key'], 'consumer_secret')
-# auth.set_access_token('access_key', 'access_secret')
-# api = tweepy.API(auth)
+auth = tweepy.OAuthHandler(keys['consumer_key'], 'consumer_secret')
+auth.set_access_token('access_key', 'access_secret')
+api = tweepy.API(auth)
 
 
-# # testing to see if this can scrape some tweets
-# def get_tweet(input, username):
-#     page = 1
-#     deadend = False
-#     while True:
-#         tweets = input.user_timeline(username, page=page)
-#         for tweet in tweets:
-#             if (datetime.datetime.now() - tweet.created_at).days < 2:
-#                 print(tweet.text.encode("utf-8"))
-#             else:
-#                 deadend = True
-#                 return
-#         if not deadend:
-#             page + 1
-#             time.sleep(500)
+# testing to see if this can scrape some tweets
+def get_tweet(input, username):
+    page = 1
+    deadend = False
+    while True:
+        tweets = input.user_timeline(username, page=page)
+        for tweet in tweets:
+            if (datetime.datetime.now() - tweet.created_at).days < 2:
+                print(tweet.text.encode("utf-8"))
+            else:
+                deadend = True
+                return
+        if not deadend:
+            page + 1
+            time.sleep(500)
 
-# # we'll use elizabeth warren's acc to test
-# get_tweet(api, "SenWarren")
+# we'll use elizabeth warren's acc to test
+get_tweet(api, "SenWarren")
 
