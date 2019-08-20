f3a041745cfdbacc0994c214e39c01d7b3ff8831
VKong6019 <vkong6019@gmail.com>
2019-08-20 00:41:41 -0400

    scraped tweets are now json format

--- src/TwitterScraper.py
+++ src/TwitterScraper.py
@@ -16,32 +16,26 @@ api = tweepy.API(auth)
 
 
 # testing if function can write multiple entries in csv file
-# TODO: make the tweet in json to be able to scrape all the tweets
 # can only scrape a portion of warren's tweets rn
-def get_tweet(input, username):
-    tweets = input.user_timeline(username, count=200, include_rts=False, tweet_mode='extended')
+def get_tweet(key, username):
+    tweets = key.user_timeline(username, count=200, include_rts=False, tweet_mode='extended')
     tweet_storage = []
     tweet_storage.extend(tweets)
     old = tweet_storage[-1].id - 1
     while len(tweets) > 0:
-        tweets = input.user_timeline(username, count=200, include_rts=False, tweet_mode='extended', max_id=old)
+        tweets = key.user_timeline(username, count=200, include_rts=False, tweet_mode='extended', max_id=old)
         tweet_storage.extend(tweets)
         old = tweet_storage[-1].id - 1
-    to_csv = [[tweet.created_at, tweet.full_text.encode("utf-8")] for tweet in tweet_storage]
-    with open('tweet_data.csv', 'wb') as f:
-        to_write = csv.writer(f)
-        to_write.writerows(["created_at", "full_text"])
-        to_write.writerows(to_csv)
-    pass
 
-    # for tweet in tweets:
-    #     # if (datetime.datetime.now() - tweet.created_at).days < 3:
-    #     # edit to format for write_to_csv func
-    #     print(tweet.created_at)
-    #     print(tweet.full_text)
-    #     tweet_data.write("%s, %s\n" % (tweet.created_at, tweet.full_text.encode('utf-8')))
-    #     cnt += 1
-    #     print("%d tweets" % cnt)
+    to_csv = [json.dumps({'name': tweet.user.screen_name, 'time': tweet.created_at.__str__(), 'text': tweet.full_text})
+              for tweet in tweet_storage]
+
+    with open('tweet_data.csv', 'w') as f:
+        to_write = csv.writer(f)
+        json_data = [[json.loads(item)['name'], json.loads(item)['time'], json.loads(item)['text'].encode("utf-8")]
+                     for item in to_csv]
+        to_write.writerows(json_data)
+    pass
 
 
 # update and upgrade this so that:
@@ -75,7 +69,7 @@ def write_to_csv(text):
 
 
 # we'll use elizabeth warren's acc to test
-get_tweet(api, "SenWarren")
+get_tweet(api, "ewarren")
 
 # write tweets with user-input hashtag phrase to csv file
 # hashtag_phrase = raw_input('Hashtag phrase')
