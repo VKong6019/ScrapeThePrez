46c4e15893afa4a3ab036d6050794b7e802d8fd9
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-08-22 01:23:03 -0800

    db format adjustments and structural organization

--- src/TwitterScraper.py
+++ src/TwitterScraper.py
@@ -14,6 +14,7 @@ auth = tweepy.OAuthHandler(keys['consumer_key'], keys['consumer_secret'])
 auth.set_access_token(keys['access_key'], keys['access_secret'])
 api = tweepy.API(auth)
 
+
 # testing if function can write multiple entries in csv file
 # can only scrape a portion of warren's tweets rn
 def get_tweet(key, username):
@@ -21,48 +22,25 @@ def get_tweet(key, username):
     tweet_storage = []
     tweet_storage.extend(tweets)
     old = tweet_storage[-1].id - 1
+
+    # pull tweets from the desired Twitter user
     while len(tweets) > 0:
         tweets = key.user_timeline(username, count=200, include_rts=False, tweet_mode='extended', max_id=old)
         tweet_storage.extend(tweets)
         old = tweet_storage[-1].id - 1
 
+    # csv file sorted by columns: name, time, text
     to_csv = [json.dumps({'name': tweet.user.screen_name, 'time': tweet.created_at.__str__(), 'text': tweet.full_text})
               for tweet in tweet_storage]
 
-    with open('tweet_data.csv', 'w') as f:
+    # writes into rows as json objects
+    with open('tweet_data.csv', 'a') as f:
         to_write = csv.writer(f)
         json_data = [[json.loads(item)['name'], json.loads(item)['time'], json.loads(item)['text'].encode("utf-8")]
                      for item in to_csv]
         to_write.writerows(json_data)
     pass
 
-
-# update and upgrade this so that:
-# - columns for different fields
-# - grouped by twitter handle
-# - more than one tweet (TURTLES)
-
-def write_to_csv(text):
-    # open spreadsheet
-    tweet_data = open('tweet_data.csv', 'w')
-    with tweet_data as file:
-        tweet_data.write(text)
-
-    # we'll use elizabeth warren's acc to test
-    get_tweet(api, "SenWarren")
-    # get_tweet(api, "SenSanders")
-
-    # file = '_'.join(re.findall(r"#(\w+)", hashtag_phrase))
-    #
-    # # open spreadsheet
-    # with open('%s.csv' % (file), 'wb') as file:
-    #     w = csv.writer(file)
-    #     # write header row to spreadsheet
-    #     w.writerow(['timestamp', 'tweet_text', 'username', 'all_hashtags'])
-    #
-    #     # for each tweet matching our hashtags, write relevant info to spreadsheet
-    #     for tweet in tweepy.Cursor(api.search, q=hashtag_phrase + ' -filter:retweets',
-    #                                lang="en", tweet_mode='extended').items(100):
-    #         w.writerow([tweet.created_at, tweet.full_text.replace('\n', ' ').encode('utf-8'),
-    #                     tweet.user.screen_name.encode('utf-8'),
-    #                     [e['text'] for e in tweet.json['entities']['hashtags']]])
+# we'll use elizabeth warren's acc to test
+get_tweet(api, "SenWarren")
+get_tweet(api, "SenSanders")
--- src/main.py
+++ src/main.py
@@ -9,33 +9,36 @@ app = firebase_admin.initialize_app(cred)
 
 firestore = firestore.client()
 
+# batches data into bulks
 def batch_data(iterable, n=1):
     l = len(iterable)
     for ndx in range(0, l, n):
         yield iterable[ndx:min(ndx + n, l)]
 
-file = "tweet_data.csv"
+file = "fake_data.csv"
 candidate_tweets = "candidates"
 
 data = []
 headers = []
+
+# reads and parses csv file
 with open(file) as csv_file:
     csv_reader = csv.reader(csv_file, delimiter=',')
     line_count = 0
     for row in csv_reader:
-        if line_count == 0:
-            for header in row:
-                headers.append(header)
-            line_count += 1
-        else:
-            obj = {}
-            for idx, item in enumerate(row):
-                obj[headers[idx]] = item
-            data.append(obj)
-            line_count += 1
+    # case for recording column labels - organization
+        headers.append("name")
+        headers.append("time")
+        headers.append("text")
+        obj = {}
+        for idx, item in enumerate(row):
+            obj[headers[idx]] = item
+        data.append(obj)
+        line_count += 1
     print(f'Processed {line_count} lines.')
 
-for batched_data in batch_data(data, 200):
+# writes all data into Cloud Firestone
+for batched_data in batch_data(data, 10):
     batch = firestore.batch()
     for data_item in batched_data:
         doc_ref = firestore.collection(candidate_tweets).document()
