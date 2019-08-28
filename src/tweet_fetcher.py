f077ee0992d6a1f1e170214aabe037a9446adc7e
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-08-28 00:31:36 -0700

    searchable multi-index query

--- src/tweet_fetcher.py
+++ src/tweet_fetcher.py
@@ -5,27 +5,26 @@ cred = credentials.Certificate("./serviceAccountKeys.json")
 app = firebase_admin.initialize_app(cred)
 
 database = firestore.client()
-candidate_collection = database.collection(u'candidates').document('SenWarren').collection('tweets')
+candidate_collection = database.collection(u'candidates')
+
 
 # an attempt to query the database
 def query_tweets(keyword, candidate):
     candidate_tweets = candidate_collection.document(candidate).collection("tweets")
 
     keywordArray = parse_text(keyword)
+    print(keywordArray)
 
     # sadly uses multiple queries to search individual words in keyword
-    queryArray = []
     for word in keywordArray:
-        queryArray.append(candidate_tweets.where(u'keywords', u'array_contains', word).limit(20))
-
-    # return final query list only with complete keyword
-    for query in queryArray:
-        print(keywordArray)
-        print(query.toString)
-        if all(elem in keywordArray for elem in queryArray):
-            # grabs all tweets with keyword
-            for item in query.stream():
+        query = candidate_tweets.where(u'keywords', u'array_contains', word).limit(20)
+        for item in query.stream():
+            print(keywordArray)
+            print(item.get('keywords'))
+            if all(elem in item.get('keywords') for elem in keywordArray):
+                # grabs all tweets with keyword
                 print(item.get('text'))
+
 
 # text parser to make indexable keywords array
 def parse_text(tweet_text):
@@ -35,6 +34,5 @@ def parse_text(tweet_text):
 
     return text_array
 
-
-query_tweets("skinny", "SenWarren")
-query_tweets('Puerto', "SenSanders")
+# query_tweets("More than 2000", "SenWarren")
+# query_tweets('Puerto', "SenSanders")
