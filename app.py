ce290ace82242cedbde7a35b7c47412cf0624371
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-08-30 02:51:31 -0700

    stylized website

--- app.py
+++ app.py
@@ -2,13 +2,14 @@ from flask import Flask, render_template, request
 from src.tweet_fetcher import query_tweets
 
 # Flask server for handling HTTP requests
-app = Flask(__name__, template_folder="src/templates")
+app = Flask(__name__, template_folder="templates")
 
 
 @app.route('/', methods=['GET', 'POST'])
 def website():
     if request.method == 'POST':
         tweet = request.form['keywords']
+        print("<h2>" + tweet + " </h2>")
         return render_template('index.html',  tweet=query_tweets(tweet, "Danickyflash"))
 
     return render_template('index.html')
@@ -18,12 +19,6 @@ def website():
 # - Fix 500 HTTP error with app in JS
 # - Fix path so that it prints out correctly! (might be solved if we fix first one)
 # - make this work for linda time
-
-# @app.route('/<path:path>')
-# def searcher(path):
-#     # result = request.args.get('keywords')
-#     # not getting the keywords in the search bar
-#     return render_template('index.html', tweet=query_tweets("cool", "Danickyflash"))
 
 if __name__ == '__main__':
     app.run(debug=True)
