3ed0616154add2bbea8333d116b77e1e27ed5255
Nicole Danuwidjaja <danuwidjajanicole@gmail.com>
2019-12-21 02:25:43 -0800

    fresh new django backend

--- animations.js
+++
@@ -1,8 +0,0 @@
-$("#bar").click(function() {
-    var id = $(this).attr("href");
-    var offset = 60;
-    var target = $(id).offset().top - offset;
-    $('html, body').animate({scrollTop:target}, 500);
-    event.preventDefault();
-    });
-});
---
+++ scrapetheserver/README.md
@@ -0,0 +1,8 @@
+# Back-end
+Django server
+
+## Usage
+`python manage.py runserver`
+
+Runs the app in the development mode.<br />
+Open [http://127.0.0.1:8000/](http://127.0.0.1:8000/) to view it in the browser.
